008a49d0b7bb20c7434a74745d0089a0a6b4e3ad
Ubuntu <ubuntu@ip-10-0-1-147.ap-northeast-1.compute.internal>
2021-07-14 05:25:57 +0900

    added list_url function.

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ def load_url(key)
 end
 
 def save_url(url)
-  raise 'not support url' unless url.include?('rarethief')
+  raise 'not support url' unless url.include?('https://maps.seaofthieves.rarethief.com/index.html')
 
   key = OpenSSL::Digest::SHA1.hexdigest(url)
   insert_sql = 'INSERT INTO easy_url VALUES(?, ?)'
@@ -52,6 +52,16 @@ def save_url(url)
   key
 end
 
+def list_url
+  html = ['<table>', '<tr>', '<th>key</th>', '<th>url</th>', '</tr>']
+  select_sql = 'SELECT hash, url FROM easy_url'
+  db.execute(select_sql) do |row|
+    html << "<tr><td>#{row[0]}</td><td><a href=\"#{row[1]}\">url</a></td></tr>"
+  end
+  html << '</table>'
+  html.join
+end
+
 # sample
 # key = save_url('https://maps.seaofthieves.rarethief.com/index.html?marker=landmark|Landmark|Barrel%20cart%20to%20the%20West|-6135.2529296875_7673.666015625')
 # puts load_url(key)
@@ -59,6 +69,7 @@ end
 get '/' do
   key = save_url(params['url']) unless params['url'].nil?
   redirect load_url(params['key']), 302 unless params['key'].nil?
+  list = list_url unless params['list'].nil?
   html = <<~HTML
     <html>
     <head><title>短縮URL作成</title></head><body>
@@ -70,6 +81,7 @@ get '/' do
     <br />
     #{key ? '<a href="https://sot.nagonago.tv/cgi/url/index.rb?key=' + key + '">Copy me</a>' : ''}
     </form>
+    #{list}
     </body></html>
   HTML
 end
