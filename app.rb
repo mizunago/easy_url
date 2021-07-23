798fa303e07190ca2e23663a55de07ae691cdcc1
Ubuntu <ubuntu@ip-10-0-1-147.ap-northeast-1.compute.internal>
2021-07-24 02:38:48 +0900

    すでに登録済みであることを表示するように修正

--- app.rb
+++ app.rb
@@ -34,22 +34,32 @@ def load_url(key)
   nil
 end
 
-def save_url(url)
+def save_url(key, url)
   raise 'not support url' unless url.include?('https://maps.seaofthieves.rarethief.com/index.html')
 
-  key = OpenSSL::Digest::SHA1.hexdigest(url)
   insert_sql = 'INSERT INTO easy_url VALUES(?, ?)'
+  db.execute(insert_sql, key, url)
+  key
+end
+
+def save_url_wrap(url)
+  key = OpenSSL::Digest::SHA1.hexdigest(url)
+  is_new = true
   begin
-    db.execute(insert_sql, key, url)
+    save_url(key, url)
   rescue SQLite3::ConstraintException
     # 重複した場合に同じ URL か確認する
     select_sql = 'SELECT url FROM easy_url where hash = ?'
     # 同じ URL なら問題ないので抜ける
     db.execute(select_sql, key) do |row|
-      raise "SHA1 is conflict.\nkey:#{key}\ndb value:     #{row[0]}\ninsert value: #{url}" if row[0] != url
+      if row[0] == url
+        is_new = false
+      else
+        raise "SHA1 is conflict.\nkey:#{key}\ndb value:     #{row[0]}\ninsert value: #{url}"
+      end
     end
   end
-  key
+  [key, is_new]
 end
 
 def list_url
@@ -67,7 +77,7 @@ end
 # puts load_url(key)
 
 get '/' do
-  key = save_url(params['url']) unless params['url'].nil?
+  key, is_new = save_url_wrap(params['url']) unless params['url'].nil?
   redirect load_url(params['key']), 302 unless params['key'].nil?
   list = list_url unless params['list'].nil?
   html = <<~HTML
@@ -81,6 +91,7 @@ get '/' do
     <br />
     #{key ? '<a href="https://sot.nagonago.tv/cgi/url/index.rb?key=' + key + '">Copy me</a>' : ''}
     </form>
+    #{is_new ? '新規登録' : 'すでに登録済み'}<br />
     #{list}
     </body></html>
   HTML
